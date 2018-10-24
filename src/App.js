4f04aa0e8014a58430b0bfeb7ed3a71cd511a0a5
Timothy Hoang <timh1203@gmail.com>
2018-10-24 10:19:24 -0400

    Add enzyme and test with child component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChildComponent from './ChildComponent.js'
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React and Beyond
           </a>
+          <ChildComponent />
         </header>
       </div>
     );
---
+++ src/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChildComponent() {
+  return (
+    <div>
+      <h1>Child Component</h1>
+    </div>
+  )
+}
+
+export default ChildComponent
